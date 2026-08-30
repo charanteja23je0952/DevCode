8959a66a719e750c9dd88f07b1bfecebe3e112c8
charanteja23je0952 <23je0952@iitism.ac.in>
2026-08-30 16:19:07 +0530

    cleaned up mern-task-manager

--- backend/snapshots/base/mern_task_manager/backend/utils/generateToken.js
+++ backend/snapshots/base/mern_task_manager/backend/utils/generateToken.js
@@ -1,10 +1,5 @@
 import jwt from "jsonwebtoken";
 
-
-// const domain =
-//   process.env.NODE_ENV === "production" ? `.onrender.com` : "localhost";
-
-
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "1d",
@@ -12,26 +7,13 @@ const generateToken = (res, userId) => {
 
   const isProduction = process.env.NODE_ENV === "production";
 
-  //   res.cookie("jwt", token, {
-  //     httpOnly: true,
-  //     domain,
-  //     signed: true,
-  //     path: "/",
-  //     secure: true,
-  //     sameSite: "None",
-  //     maxAge: 1 * 24 * 60 * 60 * 1000, //1 day
-  //   });
-
-  //   return token;
-  // };
-
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: isProduction, // Use secure flag in production
-    sameSite: isProduction ? "None" : "Lax", // Use 'None' for cross-site cookies in production
-    domain: isProduction ? "onrender.com" : undefined, // Set domain only in production
+    secure: isProduction,
+    sameSite: isProduction ? "None" : "Lax",
+    domain: isProduction ? "onrender.com" : undefined,
     path: "/",
-    maxAge: 24 * 60 * 60 * 1000, // 1 day
+    maxAge: 24 * 60 * 60 * 1000,
   });
 
   return token;
